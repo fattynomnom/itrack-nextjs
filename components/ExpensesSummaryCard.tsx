ac4076e997b96ddb64fc6982b894eb50f26c2984
Madeleine Cheng Mei Tee <madeleinetee@hotmail.com>
2023-10-13 16:55:23 +0800

    feat: ExpensesSummaryCard

--- components/ExpensesSummaryCard.tsx
+++ components/ExpensesSummaryCard.tsx
@@ -1,4 +1,7 @@
-import { Card, Color, DonutChart, Text, Title } from '@tremor/react'
+import { Button, Card, Color, DonutChart, Text, Title } from '@tremor/react'
+
+import { ChevronDoubleRightIcon } from '@heroicons/react/24/solid'
+import { useRouter } from 'next/router'
 
 export default function ExpensesSummaryCard({
     categories
@@ -10,9 +13,11 @@ export default function ExpensesSummaryCard({
         colorName: Color
     }[]
 }) {
+    const router = useRouter()
+
     return (
         <Card className="col-span-full">
-            <Title>Expenses breakdown</Title>
+            <Title>Top 3 expense categories</Title>
             <div className="flex space-x-5 mt-5">
                 <div className="h-44 w-44">
                     <DonutChart
@@ -23,24 +28,42 @@ export default function ExpensesSummaryCard({
                         valueFormatter={(amount: number) => `$ ${amount} (10%)`}
                     />
                 </div>
-                <ul className="flex-1 px-5 divide-y flex flex-col justify-center">
-                    {categories.map(({ label, amount, color }) => (
-                        <li key={label} className="py-3 flex justify-between">
-                            <div className="flex space-x-3">
-                                <div
-                                    className="w-5 h-5 rounded-full"
-                                    style={{
-                                        backgroundColor: color as string
-                                    }}
-                                />
-                                <Text>{label}</Text>
-                            </div>
-                            <Text className="tabular-nums">
-                                $ {amount} (10%)
-                            </Text>
-                        </li>
-                    ))}
-                </ul>
+                <div className="flex-1 px-5 flex flex-col justify-center space-y-3">
+                    <ul className="divide-y">
+                        {categories
+                            .slice(0, 3)
+                            .map(({ label, amount, color }) => (
+                                <li
+                                    key={label}
+                                    className="py-3 flex justify-between"
+                                >
+                                    <div className="flex space-x-3">
+                                        <div
+                                            className="w-5 h-5 rounded-full"
+                                            style={{
+                                                backgroundColor: color as string
+                                            }}
+                                        />
+                                        <Text>{label}</Text>
+                                    </div>
+                                    <Text className="tabular-nums">
+                                        $ {amount} (10%)
+                                    </Text>
+                                </li>
+                            ))}
+                    </ul>
+                    <div className="text-right">
+                        <Button
+                            variant="light"
+                            size="xs"
+                            iconPosition="right"
+                            icon={ChevronDoubleRightIcon}
+                            onClick={() => router.push('/expenses')}
+                        >
+                            View all
+                        </Button>
+                    </div>
+                </div>
             </div>
         </Card>
     )
